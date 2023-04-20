df8ec8d13a195fdf3be9a8a76a2f7f6941f6b063
Manuel Ulate <fr3zs09@gmail.com>
2023-04-19 21:09:13 -0600

    placesProvider and indexts

--- src/context/places/PlacesProvider.tsx
+++ src/context/places/PlacesProvider.tsx
@@ -1,3 +1,4 @@
+import { PlacesContext } from "./PlacesContext";
 
 export interface PlacesState {
     isLoading    : boolean;
@@ -11,6 +12,11 @@ const INITIAL_STATE:PlacesState = {
 
 export const PlacesProvider = () => {
   return (
-    <div>PlacesProvider</div>
+    <PlacesContext.Provider value={{ 
+        isLoading: true, userLocation: undefined 
+    }}
+    >
+
+    </PlacesContext.Provider>
   )
 }
