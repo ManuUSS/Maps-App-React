65a224b2c184f34b9857836a03da6f60518c3cb9
Manuel Ulate <fr3zs09@gmail.com>
2023-04-20 11:40:16 -0600

    search-bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,6 +2,12 @@
 
 export const SearchBar = () => {
   return (
-    <div className="search-container"></div>
+    <div className="search-container">
+        <input 
+            type="text" 
+            className="form-control"
+            placeholder="Buscar lugar"
+        />
+    </div>
   )
 }
