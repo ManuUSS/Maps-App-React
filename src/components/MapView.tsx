bdbb07952d07323f10bbcca8b38e83fe9d17ae7b
Manuel Ulate <fr3zs09@gmail.com>
2023-04-20 11:23:16 -0600

    map set on context

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -1,23 +1,25 @@
 import { useContext, useLayoutEffect, useRef } from 'react';
 import { Map } from 'mapbox-gl';
-import { PlacesContext } from '../context';
+import { MapContext, PlacesContext } from '../context';
 import { Loading } from './Loading';
 
 
 export const MapView = () => {
 
-    const { isLoading, userLocation } = useContext( PlacesContext );
+    const { isLoading, userLocation } = useContext( PlacesContext );    
+    const { setMap } = useContext( MapContext );    
     const mapDiv = useRef<HTMLDivElement>( null );
 
     useLayoutEffect(() => {
       
         if( !isLoading ) {
-            new Map({
+            const map = new Map({
                 container: mapDiv.current!, 
                 style: 'mapbox://styles/mapbox/streets-v12', 
                 center: userLocation, 
                 zoom: 14, 
             });
+            setMap( map );
         }
 
     }, [ isLoading ])
