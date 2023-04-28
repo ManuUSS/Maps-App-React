e8071bc66fb202776e98d4a471a2dec27c2c8b37
Manuel Ulate <fr3zs09@gmail.com>
2023-04-27 22:09:26 -0600

    new markers added

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,17 +1,20 @@
-import { useReducer } from 'react';
+import { useContext, useEffect, useReducer } from 'react';
 import { Map, Marker, Popup } from 'mapbox-gl';
 import { MapContext } from './MapContext';
 import { mapReducer } from './mapReducer';
+import { PlacesContext } from '../places/PlacesContext';
 
 
 export interface MapState {
     isMapReady: boolean;
     map?      : Map;
+    markers   : Marker[];
 }
 
 const INITIAL_STATE:MapState = {
     isMapReady: false,
-    map: undefined
+    map: undefined,
+    markers: [],
 }
 
 interface Props {
@@ -20,7 +23,29 @@ interface Props {
 
 export const MapProvider = ({ children }:Props) => {
 
-    const [state, dispatch] = useReducer( mapReducer, INITIAL_STATE );
+    const [ state, dispatch ] = useReducer( mapReducer, INITIAL_STATE );
+    const { places } = useContext( PlacesContext );
+
+    useEffect(() => {
+        state.markers.forEach( marker => marker.remove() );
+        const newMarkers: Marker[] = [];
+        for (const place of places ) {
+            const [ lng, lat ] = place.center;
+            const popUp = new Popup()
+                .setHTML(`
+                    <h6>${ place.text_es }</h6>
+                    <p>${ place.place_name_es }</p>
+                `);
+
+            const newMarker = new Marker()
+                .setPopup( popUp )
+                .setLngLat([ lng, lat ])
+                .addTo( state.map! );
+            
+            newMarkers.push( newMarker );
+        }
+    }, [ places ])
+    
 
     const setMap = ( map: Map ) => {
 
