1cb765fdfdd927fbda1a95a94f3f022c2d1af20e
Manuel Ulate <fr3zs09@gmail.com>
2023-04-27 21:52:46 -0600

    reading places context

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PlacesContext } from '../context'
 
 export const SearchResults = () => {
+
+    const { places, isLoadingPlaces } = useContext( PlacesContext );
+
   return (
     <ul className='list-group mt-3'>
-        <li className='list-group-item list-group-item-action'>
-            <h6>Lugar</h6>
-            <p 
-                className='text-muted' 
-                style={{ fontSize: '12px' }}
-            >
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae praesentium laboriosam soluta fuga repellendus, enim modi reprehenderit nemo quia facere at esse ipsam, amet hic voluptatibus nihil nam mollitia illum!
-            </p>
-            <button className='btn btn-outline-primary'>Direcciones</button>
-        </li>
+        {
+            places.map( ( place ) => (
+                <li 
+                    key={ place.id }
+                    className='list-group-item list-group-item-action'>
+                    <h6>Lugar</h6>
+                    <p 
+                        className='text-muted' 
+                        style={{ fontSize: '12px' }}
+                    >
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae praesentium laboriosam soluta fuga repellendus, enim modi reprehenderit nemo quia facere at esse ipsam, amet hic voluptatibus nihil nam mollitia illum!
+                    </p>
+                    <button className='btn btn-outline-primary btn-sm'>Direcciones</button>
+                </li>
+            ))
+        }
     </ul>
   )
 }
