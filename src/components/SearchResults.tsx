8f6f8e5d08e441f2a2ee0d72eb24933746dbba2b
Manuel Ulate <fr3zs09@gmail.com>
2023-04-27 21:55:41 -0600

    isLoading show different component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -5,24 +5,32 @@ export const SearchResults = () => {
 
     const { places, isLoadingPlaces } = useContext( PlacesContext );
 
-  return (
-    <ul className='list-group mt-3'>
-        {
-            places.map( ( place ) => (
-                <li 
-                    key={ place.id }
-                    className='list-group-item list-group-item-action'>
-                    <h6>Lugar</h6>
-                    <p 
-                        className='text-muted' 
-                        style={{ fontSize: '12px' }}
-                    >
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae praesentium laboriosam soluta fuga repellendus, enim modi reprehenderit nemo quia facere at esse ipsam, amet hic voluptatibus nihil nam mollitia illum!
-                    </p>
-                    <button className='btn btn-outline-primary btn-sm'>Direcciones</button>
-                </li>
-            ))
-        }
-    </ul>
-  )
+    if( isLoadingPlaces ) 
+        return ( 
+            <div className='alert alert-primary mt-2 text-center'>
+                <h6>Buscando...</h6>
+                <p>Espere por favor</p>
+            </div> 
+        )
+
+    return (
+        <ul className='list-group mt-3'>
+            {
+                places.map( ( place ) => (
+                    <li 
+                        key={ place.id }
+                        className='list-group-item list-group-item-action'>
+                        <h6>{ place.text }</h6>
+                        <p 
+                            className='text-muted' 
+                            style={{ fontSize: '12px' }}
+                        >
+                            { place.place_name }
+                        </p>
+                        <button className='btn btn-outline-primary btn-sm'>Direcciones</button>
+                    </li>
+                ))
+            }
+        </ul>
+    )
 }
