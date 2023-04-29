00b8ea721265d53691aa5ffa56a0554896d12881
Manuel Ulate <fr3zs09@gmail.com>
2023-04-28 21:54:16 -0600

    conditional active class

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { MapContext, PlacesContext } from '../context'
 import { LoadingPlaces } from './LoadingPlaces';
 import { Feature } from '../interfaces/places';
@@ -7,10 +7,12 @@ export const SearchResults = () => {
 
     const { places, isLoadingPlaces } = useContext( PlacesContext );
     const { map } = useContext( MapContext );
+    const [ activeId, setActiveId ] = useState('');
+
     const onPlaceClick = ( place: Feature ) => {
         
+        setActiveId( place.id );
         const [ lng, lat ] = place.center;
-        
         map?.flyTo({
             zoom: 14,
             center: [ lng, lat ]
@@ -29,10 +31,11 @@ export const SearchResults = () => {
                 places.map( ( place ) => (
                     <li 
                         key={ place.id }
-                        className='list-group-item list-group-item-action pointer'>
+                        className={`list-group-item list-group-item-action pointer ${ ( activeId === place.id ) && 'active' }`}
+                        onClick={() => onPlaceClick( place )}    
+                    >
                         <h6>{ place.text }</h6>
                         <p 
-                            className='text-muted' 
                             style={{ fontSize: '12px' }}
                         >
                             { place.place_name }
